f77ed2d77954b7d02b9c6c7d86be8443cd0056dc
OsokinNikita <110087045+OsokinNikita@users.noreply.github.com>
2023-02-17 14:39:04 +0200

    transactions-react/css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,8 @@ import data from '../data/data.json';
 import { FriendList } from './FriendList/FriendList';
 import friends from '../data/friends.json';
 
-// import transactions from '../data/transactions.json';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import transactions from '../data/transactions.json';
 
 export const App = () => {
   return (
@@ -22,6 +23,7 @@ export const App = () => {
       <Statistics title="Upload stats" stats={data} />
       {/* <Statistics stats={data} /> */}
       <FriendList friends={friends} />;
+      <TransactionHistory items={transactions} />;
     </>
   );
 };
